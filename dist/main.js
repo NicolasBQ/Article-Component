33065847bb1700653c2f4b07ec4df0c1a8ebe033
NicolasBQ <nicolasbquintero@gmail.com>
2022-02-14 21:36:49 -0500

    Article Component

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,23 @@
-const mainBtn = document.querySelector(".content-social__button");
-const shareBtn = document.querySelector(".content-share__button");
+const toggleBtn = document.querySelector(".share-button");
+const contentShare = document.querySelector(".content-share");
+const contentSocial = document.querySelector(".content-social");
+const svgPath = document.querySelector(".svg-path");
+
+const showSocials = () => {
+  if (contentSocial.classList.contains("flex-row")) {
+    contentSocial.classList.remove("flex-row");
+    contentShare.classList.add("flex-row");
+    toggleBtn.classList.add("share-button__active");
+    svgPath.classList.add("share-button__fill");
+  } else {
+    contentSocial.classList.add("flex-row");
+    contentShare.classList.remove("flex-row");
+    toggleBtn.classList.remove("share-button__active");
+    svgPath.classList.remove("share-button__fill");
+  }
+};
+
+toggleBtn.addEventListener("click", showSocials);
+
+//Arreglar Estilos contentShare
+//Desktop version contentShare.
